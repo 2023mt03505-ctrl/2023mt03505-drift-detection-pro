aa2607e091dff2096d53d647ddccdfdf2866f6f6
2023mt03505 <your-email@example.com>
2025-10-20 17:58:45 +0530

    Fix Rego syntax in nsg.rego

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
+###########################################
+# 🔹 Azure Provider + Resources
+###########################################
 provider "azurerm" {
   features {}
-  
   resource_provider_registrations = "none"
 }
 
@@ -24,7 +26,7 @@ locals {
 }
 
 # -----------------------------
-# Resource Group
+# Azure Resource Group
 # -----------------------------
 resource "azurerm_resource_group" "t" {
   name     = "rg-2023mt03505-t"
@@ -32,22 +34,19 @@ resource "azurerm_resource_group" "t" {
 }
 
 # -----------------------------
-# Storage Account
+# Azure Storage Account
 # -----------------------------
 resource "azurerm_storage_account" "sa" {
-  # must be globally unique, lowercase, 3–24 chars
   name                     = lower("st2023mt03505${var.region_index}")
   resource_group_name      = azurerm_resource_group.t.name
   location                 = azurerm_resource_group.t.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
   min_tls_version          = "TLS1_2"
-  
 }
 
 # -----------------------------
-# Storage Container
+# Azure Storage Container
 # -----------------------------
 resource "azurerm_storage_container" "secure_container" {
   name                  = "secure-container"
@@ -56,7 +55,7 @@ resource "azurerm_storage_container" "secure_container" {
 }
 
 # -----------------------------
-# Network Security Group
+# Azure Network Security Group
 # -----------------------------
 resource "azurerm_network_security_group" "secure_nsg" {
   name                = "nsg-secure"
@@ -76,21 +75,98 @@ resource "azurerm_network_security_group" "secure_nsg" {
   }
 }
 
+###########################################
+# 🔹 AWS Provider + Resources
+###########################################
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy"
+  type        = string
+  default     = "ap-south-1"
+}
+
 # -----------------------------
-# Outputs
+# AWS S3 Storage (like Azure Storage Account)
 # -----------------------------
-output "resource_group" {
+resource "aws_s3_bucket" "storage" {
+  bucket        = "st2023mt03505-${var.region_index}"
+  force_destroy = true
+
+  # Block all public access
+  acl = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Project = "MTechDrift"
+  }
+}
+
+# -----------------------------
+# AWS Security Group (like Azure NSG)
+# -----------------------------
+variable "vpc_id" {
+  description = "Existing VPC ID to attach security group"
+  type        = string
+}
+
+resource "aws_security_group" "secure_sg" {
+  name        = "secure-sg"
+  description = "Secure SG with restricted SSH"
+  vpc_id      = var.vpc_id
+
+  # Only allow internal SSH (not 0.0.0.0/0)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Project = "MTechDrift"
+  }
+}
+
+###########################################
+# 🔹 Outputs
+###########################################
+output "azure_resource_group" {
   value = azurerm_resource_group.t.name
 }
 
-output "storage_account_name" {
+output "azure_storage_account_name" {
   value = azurerm_storage_account.sa.name
 }
 
-output "storage_container_name" {
+output "azure_storage_container_name" {
   value = azurerm_storage_container.secure_container.name
 }
 
-output "nsg_name" {
+output "azure_nsg_name" {
   value = azurerm_network_security_group.secure_nsg.name
+}
+
+output "aws_s3_bucket_name" {
+  value = aws_s3_bucket.storage.bucket
+}
+
+output "aws_security_group_name" {
+  value = aws_security_group.secure_sg.name
 }
