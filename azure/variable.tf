ac76439834b5355bebaaf56cb9ed2cf0dea8474c
2023mt03505 <your-email@example.com>
2025-11-08 00:41:01 +0530

    Fix Rego syntax in nsg.rego

--- azure/variable.tf
+++ azure/variable.tf
@@ -1,20 +1,20 @@
-variable "subscription_id" {
+#variable "subscription_id" {
   description = "Azure subscription ID"
   type        = string
 }
 
-variable "client_id" {
+#variable "client_id" {
   description = "Service principal client ID"
   type        = string
 }
 
-variable "client_secret" {
+#variable "client_secret" {
   description = "Service principal client secret"
   type        = string
   sensitive   = true
 }
 
-variable "tenant_id" {
+#variable "tenant_id" {
   description = "Azure tenant ID"
   type        = string
 }
